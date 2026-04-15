1af9564fd6ca762721f76b7380f12beb2c604abf
luciaarlz <lucire03@gmail.com>
2026-04-15 20:21:05 +0200

    Fichero del modelo .onnx

--- export_model.py
+++ export_model.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import onnx
 
-# 1. Definir arquitectura idéntica
+# 1. Definir y cargar modelo (lo de siempre)
 model = models.resnet18()
 model.fc = nn.Sequential(
     nn.Dropout(p=0.4),
@@ -11,15 +12,22 @@ model.fc = nn.Sequential(
     nn.Dropout(p=0.3),
     nn.Linear(256, 5),
 )
-
-# 2. Cargar tus pesos
 model.load_state_dict(torch.load('best_model_ResNet18.pth', map_location='cpu'))
 model.eval()
 
-# 3. Exportar
+# 2. Exportar temporalmente
 dummy_input = torch.randn(1, 3, 128, 128)
-torch.onnx.export(model, dummy_input, "model.onnx", 
-                  input_names=['input'], 
-                  output_names=['output'],
-                  dynamic_axes={'input': {0: 'batch_size'}})
-print("¡Éxito! model.onnx generado.")
+torch.onnx.export(
+    model, dummy_input, "temp.onnx", 
+    export_params=True, 
+    opset_version=11,
+    input_names=['input'], 
+    output_names=['output']
+)
+
+# 3. EL TRUCO: Cargar y volver a guardar TODO en uno
+# Esto obliga a ONNX a meter los datos externos dentro del archivo principal
+modelo_cargado = onnx.load("temp.onnx")
+onnx.save_model(modelo_cargado, "model.onnx", save_as_external_data=False)
+
+print("✅ ¡HECHO! Comprueba el tamaño de 'model.onnx'. Debería pesar unos 44-45 MB.")
